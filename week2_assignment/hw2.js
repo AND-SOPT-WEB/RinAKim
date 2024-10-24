426d05393ad3dffb655362e690a924beae5cb927
Rinakk <bloomcott@u.sogang.ac.kr>
2024-10-24 23:22:11 +0900

    js 추가

--- week2_assignment/hw2.js
+++ week2_assignment/hw2.js
@@ -1,339 +1,471 @@
-export const members = [
-    {
-      id: 1,
-      name: "강민하",
-      englishName: "MinHaKang",
-      github: "m2na7",
-      gender: "male",
-      role: "YB",
-      firstWeekGroup: 6,
-      secondWeekGroup: 4,
-    },
-    {
-      id: 2,
-      name: "공준혁",
-      englishName: "JunHyukKong",
-      github: "ocahs9",
-      gender: "male",
-      role: "OB",
-      firstWeekGroup: 5,
-      secondWeekGroup: 7,
-    },
-    {
-      id: 3,
-      name: "곽지욱",
-      englishName: "JiUgGwag",
-      github: "gwagjiug",
-      gender: "male",
-      role: "YB",
-      firstWeekGroup: 9,
-      secondWeekGroup: 1,
-    },
-    {
-      id: 4,
-      name: "김가연",
-      englishName: "GaYeonKim",
-      github: "ayla_12",
-      gender: "female",
-      role: "OB",
-      firstWeekGroup: 8,
-      secondWeekGroup: 7,
-    },
-    {
-      id: 5,
-      name: "김가현",
-      englishName: "GaHyeonKim",
-      github: "maylh",
-      gender: "female",
-      role: "YB",
-      firstWeekGroup: 4,
-      secondWeekGroup: 8,
-    },
-    {
-      id: 6,
-      name: "김건휘",
-      englishName: "GeonHwiKim",
-      github: "KIMGEONHWI",
-      gender: "male",
-      role: "OB",
-      firstWeekGroup: 2,
-      secondWeekGroup: 8,
-    },
-    {
-      id: 7,
-      name: "김고은",
-      englishName: "GoEunKim",
-      github: "gonn-i",
-      gender: "female",
-      role: "YB",
-      firstWeekGroup: 2,
-      secondWeekGroup: 2,
-    },
-    {
-      id: 8,
-      name: "김다현",
-      englishName: "DaHyunKim",
-      github: "daahyunk",
-      gender: "female",
-      role: "YB",
-      firstWeekGroup: 5,
-      secondWeekGroup: 9,
-    },
-    {
-      id: 9,
-      name: "김린아",
-      englishName: "RinAKim",
-      github: "Rinakk",
-      gender: "female",
-      role: "YB",
-      firstWeekGroup: 2,
-      secondWeekGroup: 9,
-    },
-    {
-      id: 10,
-      name: "김민정",
-      englishName: "MinJeongKim",
-      github: "minjeoong",
-      gender: "female",
-      role: "YB",
-      firstWeekGroup: 4,
-      secondWeekGroup: 7,
-    },
-    {
-      id: 11,
-      name: "김채은",
-      englishName: "ChaeEunKim",
-      github: "bongtta",
-      gender: "female",
-      role: "YB",
-      firstWeekGroup: 1,
-      secondWeekGroup: 5,
-    },
-    {
-      id: 12,
-      name: "김채현",
-      englishName: "ChaeHyunKim",
-      github: "imddoy",
-      gender: "female",
-      role: "OB",
-      firstWeekGroup: 8,
-      secondWeekGroup: 1,
-    },
-    {
-      id: 13,
-      name: "김태욱",
-      englishName: "TaeWookKim",
-      github: "Taew00k",
-      gender: "male",
-      role: "YB",
-      firstWeekGroup: 8,
-      secondWeekGroup: 6,
-    },
-    {
-      id: 14,
-      name: "김한서",
-      englishName: "HanSeoKim",
-      github: "seueooo",
-      gender: "female",
-      role: "YB",
-      firstWeekGroup: 6,
-      secondWeekGroup: 3,
-    },
-    {
-      id: 15,
-      name: "노찬영",
-      englishName: "ChanYoungNoh",
-      github: "shroqkf",
-      gender: "female",
-      role: "YB",
-      firstWeekGroup: 9,
-      secondWeekGroup: 2,
-    },
-    {
-      id: 16,
-      name: "문성희",
-      englishName: "SeonghuiMoon",
-      github: "seong-hui",
-      gender: "female",
-      role: "OB",
-      firstWeekGroup: 9,
-      secondWeekGroup: 4,
-    },
-    {
-      id: 17,
-      name: "박채연",
-      englishName: "ChaeYeonPark",
-      github: "chaeneey",
-      gender: "female",
-      role: "OB",
-      firstWeekGroup: 1,
-      secondWeekGroup: 9,
-    },
-    {
-      id: 18,
-      name: "박희선",
-      englishName: "HeeSunPark",
-      github: "heesunee",
-      gender: "female",
-      role: "YB",
-      firstWeekGroup: 3,
-      secondWeekGroup: 3,
-    },
-    {
-      id: 19,
-      name: "배영경",
-      englishName: "YoungKyungBae",
-      github: "bykbyk0401",
-      gender: "female",
-      role: "YB",
-      firstWeekGroup: 6,
-      secondWeekGroup: 2,
-    },
-    {
-      id: 20,
-      name: "손지유",
-      englishName: "JiYooSon",
-      github: "zzz-myam",
-      gender: "female",
-      role: "OB",
-      firstWeekGroup: 9,
-      secondWeekGroup: 5,
-    },
-    {
-      id: 21,
-      name: "유서연",
-      englishName: "SeoYeonYu",
-      github: "youtheyeon",
-      gender: "female",
-      role: "YB",
-      firstWeekGroup: 3,
-      secondWeekGroup: 1,
-    },
-    {
-      id: 22,
-      name: "유태승",
-      englishName: "TaeSeungYoo",
-      github: "gudusol",
-      gender: "male",
-      role: "OB",
-      firstWeekGroup: 2,
-      secondWeekGroup: 2,
-    },
-    {
-      id: 23,
-      name: "이예림",
-      englishName: "YeRimLee",
-      github: "yarimu",
-      gender: "female",
-      role: "OB",
-      firstWeekGroup: 4,
-      secondWeekGroup: 6,
-    },
-    {
-      id: 24,
-      name: "이윤지",
-      englishName: "YoonJiLee",
-      github: "Leeyoonji23",
-      gender: "female",
-      role: "YB",
-      firstWeekGroup: 5,
-      secondWeekGroup: 4,
-    },
-    {
-      id: 25,
-      name: "이진혁",
-      englishName: "JinHyeokLee",
-      github: "constantly-dev",
-      gender: "male",
-      role: "YB",
-      firstWeekGroup: 1,
-      secondWeekGroup: 3,
-    },
-    {
-      id: 26,
-      name: "임화랑",
-      englishName: "HwaRangIm",
-      github: "thisishwarang",
-      gender: "male",
-      role: "OB",
-      firstWeekGroup: 3,
-      secondWeekGroup: 5,
-    },
-    {
-      id: 27,
-      name: "최민",
-      englishName: "MinChoi",
-      github: "Minn-Choi",
-      gender: "female",
-      role: "YB",
-      firstWeekGroup: 1,
-      secondWeekGroup: 6,
-    },
-    {
-      id: 28,
-      name: "최정완",
-      englishName: "JungWoanChoi",
-      github: "look-back-luca",
-      gender: "male",
-      role: "YB",
-      firstWeekGroup: 4,
-      secondWeekGroup: 9,
-    },
-    {
-      id: 29,
-      name: "한수정",
-      englishName: "SooJeongHan",
-      github: "hansoojeongsj",
-      gender: "female",
-      role: "YB",
-      firstWeekGroup: 8,
-      secondWeekGroup: 8,
-    },
-  ];
-  
-  if (!localStorage.getItem("membersData")) {
-    localStorage.setItem("membersData", JSON.stringify(members));
+const members = [
+  {
+    id: 1,
+    name: "강민하",
+    englishName: "MinHaKang",
+    github: "m2na7",
+    gender: "male",
+    role: "YB",
+    firstWeekGroup: 6,
+    secondWeekGroup: 4,
+  },
+  {
+    id: 2,
+    name: "공준혁",
+    englishName: "JunHyukKong",
+    github: "ocahs9",
+    gender: "male",
+    role: "OB",
+    firstWeekGroup: 5,
+    secondWeekGroup: 7,
+  },
+  {
+    id: 3,
+    name: "곽지욱",
+    englishName: "JiUgGwag",
+    github: "gwagjiug",
+    gender: "male",
+    role: "YB",
+    firstWeekGroup: 9,
+    secondWeekGroup: 1,
+  },
+  {
+    id: 4,
+    name: "김가연",
+    englishName: "GaYeonKim",
+    github: "ayla-12",
+    gender: "female",
+    role: "OB",
+    firstWeekGroup: 8,
+    secondWeekGroup: 7,
+  },
+  {
+    id: 5,
+    name: "김가현",
+    englishName: "GaHyeonKim",
+    github: "maylh",
+    gender: "female",
+    role: "YB",
+    firstWeekGroup: 4,
+    secondWeekGroup: 8,
+  },
+  {
+    id: 6,
+    name: "김건휘",
+    englishName: "GeonHwiKim",
+    github: "KIMGEONHWI",
+    gender: "male",
+    role: "OB",
+    firstWeekGroup: 2,
+    secondWeekGroup: 8,
+  },
+  {
+    id: 7,
+    name: "김고은",
+    englishName: "GoEunKim",
+    github: "gonn-i",
+    gender: "female",
+    role: "YB",
+    firstWeekGroup: 2,
+    secondWeekGroup: 2,
+  },
+  {
+    id: 8,
+    name: "김다현",
+    englishName: "DaHyunKim",
+    github: "daahyunk",
+    gender: "female",
+    role: "YB",
+    firstWeekGroup: 5,
+    secondWeekGroup: 9,
+  },
+  {
+    id: 9,
+    name: "김린아",
+    englishName: "RinAKim",
+    github: "Rinakk",
+    gender: "female",
+    role: "YB",
+    firstWeekGroup: 2,
+    secondWeekGroup: 9,
+  },
+  {
+    id: 10,
+    name: "김민정",
+    englishName: "MinJeongKim",
+    github: "minjeoong",
+    gender: "female",
+    role: "YB",
+    firstWeekGroup: 4,
+    secondWeekGroup: 7,
+  },
+  {
+    id: 11,
+    name: "김채은",
+    englishName: "ChaeEunKim",
+    github: "bongtta",
+    gender: "female",
+    role: "YB",
+    firstWeekGroup: 1,
+    secondWeekGroup: 5,
+  },
+  {
+    id: 12,
+    name: "김채현",
+    englishName: "ChaeHyunKim",
+    github: "imddoy",
+    gender: "female",
+    role: "OB",
+    firstWeekGroup: 8,
+    secondWeekGroup: 1,
+  },
+  {
+    id: 13,
+    name: "김태욱",
+    englishName: "TaeWookKim",
+    github: "Taew00k",
+    gender: "male",
+    role: "YB",
+    firstWeekGroup: 8,
+    secondWeekGroup: 6,
+  },
+  {
+    id: 14,
+    name: "김한서",
+    englishName: "HanSeoKim",
+    github: "seueooo",
+    gender: "female",
+    role: "YB",
+    firstWeekGroup: 6,
+    secondWeekGroup: 3,
+  },
+  {
+    id: 15,
+    name: "노찬영",
+    englishName: "ChanYoungNoh",
+    github: "shroqkf",
+    gender: "female",
+    role: "YB",
+    firstWeekGroup: 9,
+    secondWeekGroup: 2,
+  },
+  {
+    id: 16,
+    name: "문성희",
+    englishName: "SeonghuiMoon",
+    github: "seong-hui",
+    gender: "female",
+    role: "OB",
+    firstWeekGroup: 9,
+    secondWeekGroup: 4,
+  },
+  {
+    id: 17,
+    name: "박채연",
+    englishName: "ChaeYeonPark",
+    github: "chaeneey",
+    gender: "female",
+    role: "OB",
+    firstWeekGroup: 1,
+    secondWeekGroup: 9,
+  },
+  {
+    id: 18,
+    name: "박희선",
+    englishName: "HeeSunPark",
+    github: "heesunee",
+    gender: "female",
+    role: "YB",
+    firstWeekGroup: 3,
+    secondWeekGroup: 3,
+  },
+  {
+    id: 19,
+    name: "배영경",
+    englishName: "YoungKyungBae",
+    github: "bykbyk0401",
+    gender: "female",
+    role: "YB",
+    firstWeekGroup: 6,
+    secondWeekGroup: 2,
+  },
+  {
+    id: 20,
+    name: "손지유",
+    englishName: "JiYooSon",
+    github: "zzz-myam",
+    gender: "female",
+    role: "OB",
+    firstWeekGroup: 9,
+    secondWeekGroup: 5,
+  },
+  {
+    id: 21,
+    name: "유서연",
+    englishName: "SeoYeonYu",
+    github: "youtheyeon",
+    gender: "female",
+    role: "YB",
+    firstWeekGroup: 3,
+    secondWeekGroup: 1,
+  },
+  {
+    id: 22,
+    name: "유태승",
+    englishName: "TaeSeungYoo",
+    github: "gudusol",
+    gender: "male",
+    role: "OB",
+    firstWeekGroup: 2,
+    secondWeekGroup: 2,
+  },
+  {
+    id: 23,
+    name: "이예림",
+    englishName: "YeRimLee",
+    github: "yarimu",
+    gender: "female",
+    role: "OB",
+    firstWeekGroup: 4,
+    secondWeekGroup: 6,
+  },
+  {
+    id: 24,
+    name: "이윤지",
+    englishName: "YoonJiLee",
+    github: "Leeyoonji23",
+    gender: "female",
+    role: "YB",
+    firstWeekGroup: 5,
+    secondWeekGroup: 4,
+  },
+  {
+    id: 25,
+    name: "이진혁",
+    englishName: "JinHyeokLee",
+    github: "constantly-dev",
+    gender: "male",
+    role: "YB",
+    firstWeekGroup: 1,
+    secondWeekGroup: 3,
+  },
+  {
+    id: 26,
+    name: "임화랑",
+    englishName: "HwaRangIm",
+    github: "thisishwarang",
+    gender: "male",
+    role: "OB",
+    firstWeekGroup: 3,
+    secondWeekGroup: 5,
+  },
+  {
+    id: 27,
+    name: "최민",
+    englishName: "MinChoi",
+    github: "Minn-Choi",
+    gender: "female",
+    role: "YB",
+    firstWeekGroup: 1,
+    secondWeekGroup: 6,
+  },
+  {
+    id: 28,
+    name: "최정완",
+    englishName: "JungWoanChoi",
+    github: "look-back-luca",
+    gender: "male",
+    role: "YB",
+    firstWeekGroup: 4,
+    secondWeekGroup: 9,
+  },
+  {
+    id: 29,
+    name: "한수정",
+    englishName: "SooJeongHan",
+    github: "hansoojeongsj",
+    gender: "female",
+    role: "YB",
+    firstWeekGroup: 8,
+    secondWeekGroup: 8,
+  },
+];
+
+if (!localStorage.getItem("membersData")) {
+  localStorage.setItem("membersData", JSON.stringify(members));
+  window.onload = function () {
+    console.log("loaded all members")
+    loadMembers();
+  };
 }
 
-function showFilteredMembers() {
-    // localStorage에서 members 데이터를 가져옴
+// members 데이터를 localStorage에서 불러와 테이블에 표시하는 함수
+function loadMembers() {
+  const members = JSON.parse(localStorage.getItem("membersData")) || [];
+
+  console.log("loadMembers")
+
+  let outputHTML = "";
+
+  members.forEach((member,index) => {
+      outputHTML += `<tr>
+          <td><input type="checkbox" data-index="${index}"></td>
+          <td>${member.name}</td>
+          <td>${member.englishName}</td>
+          <td>${member.github}</td>
+          <td>${member.gender}</td>
+          <td>${member.role}</td>
+          <td>${member.firstWeekGroup}</td>
+          <td>${member.secondWeekGroup}</td>
+      </tr>`;
+  });
+
+  document.querySelector("tbody").innerHTML = outputHTML;
+}
+
+// 검색 결과를 보여주는 함수
+function showFilteredTable(event) {
+  // 폼 제출 방지
+  event.preventDefault();
+
+  const members = JSON.parse(localStorage.getItem("membersData")) || [];
+
+  console.log("검색 버튼이 눌렸습니다.");
+
+  // 검색 필드 값들을 가져옴
+  const nameInput = document.getElementById("name").value.toLowerCase();
+  const englishNameInput = document.getElementById("englishName").value.toLowerCase();
+  const githubInput = document.getElementById("github").value.toLowerCase();
+  const genderInput = document.getElementById("gender").value;
+  const roleInput = document.getElementById("role").value;
+  const firstGroupInput = document.getElementById("firstGrade").value;
+  const secondGroupInput = document.getElementById("secondGrade").value;
+  
+  // 필터링된 결과를 저장할 배열
+  let filteredMembers = members.filter(member => {
+      const matchesName = nameInput === "" || member.name.toLowerCase().includes(nameInput);
+      const matchesEnglishName = englishNameInput === "" || member.englishName.toLowerCase().includes(englishNameInput);
+      const matchesGithub = githubInput === "" || member.github.toLowerCase().includes(githubInput);
+      const matchesGender = genderInput === "" || member.gender === genderInput;
+      const matchesRole = roleInput === "" || member.role === roleInput;
+      const matchesFirstGroup = firstGroupInput === "" || member.firstWeekGroup == firstGroupInput;
+      const matchesSecondGroup = secondGroupInput === "" || member.secondWeekGroup == secondGroupInput;
+
+      // 입력된 필드만 필터링에 사용
+      return matchesName && matchesEnglishName && matchesGithub && matchesGender && matchesRole && matchesFirstGroup && matchesSecondGroup;
+  });
+  let outputHTML = "";
+  // 결과를 출력하는 HTML 생성
+  filteredMembers.forEach((member,index) => {
+      outputHTML += `<tr>
+                      <td><input type="checkbox" data-index="${index}"></td>
+                      <td>${member.name}</td>
+                      <td>${member.englishName}</td>
+                      <td>${member.github}</td>
+                      <td>${member.gender}</td>
+                      <td>${member.role}</td>
+                      <td>${member.firstWeekGroup}</td>
+                      <td>${member.secondWeekGroup}</td>
+                    </tr>`;
+  });
+
+  // 결과를 출력
+  document.querySelector("tbody").innerHTML = outputHTML;
+}
+
+function resetTable(event) {
+  // 폼 제출 방지
+  event.preventDefault();
+
+  // 입력 필드 초기화
+  document.getElementById("name").value = "";
+  document.getElementById("englishName").value = "";
+  document.getElementById("github").value = "";
+  document.getElementById("gender").value = "";  // 기본값으로 설정
+  document.getElementById("role").value = "";    // 기본값으로 설정
+  document.getElementById("firstGrade").value = "";
+  document.getElementById("secondGrade").value = "";
+
+  // 콘솔 로그로 초기화 확인
+  console.log("초기화 완료");
+
+  // 필터된 테이블 출력 함수 호출
+  showFilteredTable(event);
+}
+
+// 모달 열기
+function openModal() {
+  document.getElementById("memberModal").style.display = "block";
+}
+
+// 모달 닫기
+function closeModal() {
+  document.getElementById("memberModal").style.display = "none";
+}
+
+// 새 멤버 추가
+function addNewMember(event) {
+  event.preventDefault(); // 폼 제출 방지
+
+  // 입력된 값 가져오기
+  const name = document.getElementById("newName").value;
+  const englishName = document.getElementById("newEnglishName").value;
+  const github = document.getElementById("newGithub").value;
+  const gender = document.getElementById("newGender").value;
+  const role = document.getElementById("newRole").value;
+  const firstGroup = document.getElementById("newFirstGroup").value;
+  const secondGroup = document.getElementById("newSecondGroup").value;
+
+  // 새 멤버 객체 생성
+  const newMember = {
+      name: name,
+      englishName: englishName,
+      github: github,
+      gender: gender,
+      role: role,
+      firstWeekGroup: firstGroup,
+      secondWeekGroup: secondGroup
+  };
+
+  // 기존 멤버 리스트 가져오기
+  const members = JSON.parse(localStorage.getItem("membersData")) || [];
+
+  // 새 멤버 추가
+  members.push(newMember);
+
+  // 로컬스토리지에 저장
+  localStorage.setItem("membersData", JSON.stringify(members));
+
+  // 모달 닫기
+  closeModal();
+
+  // 멤버 리스트 업데이트 (loadMembers 함수 사용)
+  loadMembers();
+}
+
+// 전체 선택 체크박스 이벤트 처리
+function toggleCheckboxes(selectAllCheckbox) {
+  // 모든 체크박스 요소 선택
+  const checkboxes = document.querySelectorAll('#memberTableBody input[type="checkbox"]');
+
+  // 헤더 체크박스의 상태에 따라 모든 체크박스의 상태를 변경
+  checkboxes.forEach(checkbox => {
+      checkbox.checked = selectAllCheckbox.checked;
+  });
+}
+
+// 선택된 항목 삭제
+function deleteSelectedMembers() {
     const members = JSON.parse(localStorage.getItem("membersData")) || [];
+    const checkboxes = document.querySelectorAll('#memberTableBody input[type="checkbox"]:checked');
 
-    // 검색 필드 값들을 가져옴
-    const nameInput = document.getElementById("name").value.toLowerCase();
-    const englishNameInput = document.getElementById("englishName").value.toLowerCase();
-    const githubInput = document.getElementById("github").value.toLowerCase();
-    const genderInput = document.getElementById("gender").value;
-    const roleInput = document.getElementById("role").value;
-    const firstGroupInput = document.getElementById("firstWeekGroup").value;
-    const secondGroupInput = document.getElementById("secondWeekGroup").value;
-
-    // 필터링된 결과를 저장할 배열
-    let filteredMembers = members.filter(member => {
-        const matchesName = nameInput === "" || member.name.toLowerCase().includes(nameInput);
-        const matchesEnglishName = englishNameInput === "" || member.englishName.toLowerCase().includes(englishNameInput);
-        const matchesGithub = githubInput === "" || member.github.toLowerCase().includes(githubInput);
-        const matchesGender = genderInput === "" || member.gender === genderInput;
-        const matchesRole = roleInput === "" || member.role === roleInput;
-        const matchesFirstGroup = firstGroupInput === "" || member.firstWeekGroup == firstGroupInput;
-        const matchesSecondGroup = secondGroupInput === "" || member.secondWeekGroup == secondGroupInput;
-
-        return matchesName && matchesEnglishName && matchesGithub && matchesGender && matchesRole && matchesFirstGroup && matchesSecondGroup;
+    // 체크된 항목의 인덱스를 찾아 해당 멤버 삭제
+    checkboxes.forEach(checkbox => {
+        const index = checkbox.getAttribute("data-index");
+        members.splice(index, 1); // 인덱스에 해당하는 멤버 삭제
     });
 
-    let outputHTML = "<table border='1'><tr><th>이름</th><th>영문이름</th><th>GitHub</th><th>성별</th><th>역할</th><th>1주차 금잔디조</th><th>2주차 금잔디조</th></tr>";
-    filteredMembers.forEach(member => {
-        outputHTML += `<tr>
-                        <td>${member.name}</td>
-                        <td>${member.englishName}</td>
-                        <td>${member.github}</td>
-                        <td>${member.gender}</td>
-                        <td>${member.role}</td>
-                        <td>${member.firstWeekGroup}</td>
-                        <td>${member.secondWeekGroup}</td>
-                      </tr>`;
-    });
-    outputHTML += "</table>";
-
-    document.getElementById("output").innerHTML = outputHTML;
+    // 로컬 스토리지 업데이트
+    localStorage.setItem("membersData", JSON.stringify(members));
+    loadMembers(); // 업데이트된 리스트 다시 로드
 }
